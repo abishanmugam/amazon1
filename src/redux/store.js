47f8cbcd70b60c15fae32183bcadc1a554f7a352
Mukesh Selvaraj <abirami@ishamistygreen.com>
2023-01-02 10:10:06 +0530

    created amazon

---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './reducers/cart';
+import wishlistReducer from './reducers/wishlist';
+
+export default configureStore({
+  reducer: {
+    cart: cartReducer,
+    wishlist: wishlistReducer,
+  },
+});
